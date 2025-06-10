8252a309d37328c2cedc6ebda06963bfd361066c
Aakash Attri <aakashattri516@gmail.com>
2025-06-10 08:46:47 +0530

    pushing day1 with some changes

--- day1/pb3.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class pb3 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int t =  sc.nextInt() ;
-        System.out.println ( (t * 9 / 5.0 )  + 32 )  ;
-    }
-}
