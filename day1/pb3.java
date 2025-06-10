68aeda2c1725e22dd418c57859844c01c95e52ed
Aakash Attri <aakashattri516@gmail.com>
2025-06-10 08:31:34 +0530

    pushing the folder

---
+++ day1/pb3.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class pb3 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t =  sc.nextInt() ;
+        System.out.println ( (t * 9 / 5.0 )  + 32 )  ;
+    }
+}
